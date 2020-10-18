c930934b8097ea5a3e5a2e5d6e1dd20443b45ebc
maryamradd <maryam.h.alraddadi@gmail.com>
2020-10-18 09:39:04 +0300

    configure testing

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,10 +1,11 @@
-import os
+from os import environ
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 
-from flaskr import create_app
-from models import setup_db, Question, Category
+
+from flaskr import create_app, db
+from flaskr.models import Question, Category
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -13,10 +14,11 @@ class TriviaTestCase(unittest.TestCase):
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app()
+        self.app.config["TESTING"] = True
+        self.app.config["DEBUG"] = False
+        self.app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("TEST_DATABASE_URI")
+
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        setup_db(self.app, self.database_path)
 
         # binds the app to the current context
         with self.app.app_context():
@@ -24,15 +26,23 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
-        pass
+        # self.app_context.pop()
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+    def test_get_paginated_questions(self):
+        """
+        tests getting all questions with pagination
+        """
+
+        response = self.client().get("/questions")
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertIn("questions", data)
+        self.assertIn("total_questions", data)
 
 
 # Make the tests conveniently executable
